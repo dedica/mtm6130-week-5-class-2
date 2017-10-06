33a278e0aabff743ce925854150d604f3d698876
Amel Dedic <dedica@algonquincollege.com>
2017-10-06 14:00:31 -0400

    updated

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/*global alert*/
+alert("Hello,\n" +
+      "It is your web browser. " +
+      "You wrote a line of JavaScript code " +
+      "telling me to print this message in the popup, so here it is!");
